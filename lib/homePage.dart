e9dcc3353c719a8d978abe460a622a257e0f8bcc
sanjit <sanjitkeleng5@gmai.com>
2024-11-07 08:27:03 -0500

    app bar

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
-     return Scaffold();
+     return Scaffold(
+        appBar: AppBar(
+           centerTitle: true,
+          title: Text('branch'),
+        ),
+     );
   }
 }
